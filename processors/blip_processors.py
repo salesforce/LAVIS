066092e59fe93e845f901f0967b0d35a257047c3
Dongxu Li <dongxu_li@outlook.com>
2022-03-29 15:12:18 +0000

    refactor processor with build func.

--- processors/blip_processors.py
+++ processors/blip_processors.py
@@ -11,8 +11,11 @@ from processors.base_processor import BaseProcessor
 
 
 class BlipCOCOImage(BaseProcessor):
-    def __init__(self):
-        self.normalize = transforms.Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711))
+    def __init__(self, mean=None, std=None):
+        if mean is None: mean = (0.48145466, 0.4578275, 0.40821073)
+        if std is None: std = (0.26862954, 0.26130258, 0.27577711)
+
+        self.normalize = transforms.Normalize(mean, std)
 
 
 @registry.register_processor("blip_coco_ret_text")
@@ -77,14 +80,25 @@ class BlipCOCORetImageTrain(BlipCOCOImage):
 
 @registry.register_processor("blip_coco_ret_vis_eval")
 class BlipCOCORetImageEval(BlipCOCOImage):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, image_size, mean, std):
+        super().__init__(mean=mean, std=std)
 
         self.transform = transforms.Compose([
-            transforms.Resize((384,384),interpolation=InterpolationMode.BICUBIC),
+            transforms.Resize((image_size, image_size),interpolation=InterpolationMode.BICUBIC),
             transforms.ToTensor(),
             self.normalize,
             ])  
 
     def __call__(self, item):
         return self.transform(item) 
+    
+    @classmethod
+    def build_processor(cls, cfg=None):
+        if cfg is None: cfg = OmegaConf.create()
+
+        image_size = cfg.get("image_size", 384)
+
+        mean = cfg.get("mean", None)
+        std = cfg.get("std", None)
+
+        return cls(image_size=image_size, mean=mean, std=std)
