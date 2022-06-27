bda1aa2abf92b96314228084ba1266b41ebe3dd2
Dongxu Li <dongxu_li@outlook.com>
2022-06-27 03:10:12 +0000

    (refactor) minor.

--- run_scripts/albef/train/train_nlvr_albef.sh
+++ run_scripts/albef/train/train_nlvr_albef.sh
@@ -1,1 +1,1 @@
-python -m torch.distributed.run --nproc_per_node=8 train.py --cfg-path lavis/projects/albef/exp_nlvr_ft.yaml
+python -m torch.distributed.run --nproc_per_node=8 train.py --cfg-path lavis/projects/albef/train/nlvr_ft.yaml
--- run_scripts/albef/train/train_vqa_albef.sh
+++ run_scripts/albef/train/train_vqa_albef.sh
@@ -1,1 +1,1 @@
-python -m torch.distributed.run --nproc_per_node=8 train.py --cfg-path lavis/projects/albef/exp_vqa_ft.yaml
+python -m torch.distributed.run --nproc_per_node=8 train.py --cfg-path lavis/projects/albef/train/vqa_ft.yaml
