08b8e43f8711b43d5d9eec3c4b4504737c0d3926
Dongxu Li <dongxu_li@outlook.com>
2022-06-30 08:55:56 +0000

    (feat) uploaded remaining configuration and run scripts.

--- run_scripts/albef/train/train_vqa_albef.sh
+++ run_scripts/albef/train/train_vqa_albef.sh
@@ -1,1 +1,2 @@
-python -m torch.distributed.run --nproc_per_node=8 train.py --cfg-path lavis/projects/albef/train/vqa_ft.yaml
+# python -m torch.distributed.run --nproc_per_node=8 train.py --cfg-path lavis/projects/albef/train/vqa_ft.yaml
+python -m torch.distributed.run --nproc_per_node=8 train.py --cfg-path lavis/projects_release/albef/train/vqa_ft.yaml
--- run_scripts/blip/eval/eval_ret_coco.sh
+++ run_scripts/blip/eval/eval_ret_coco.sh
@@ -1,4 +1,1 @@
-cd ..
-
-# python -m torch.distributed.run --nproc_per_node=16 evaluate.py --cfg-path lavis/projects/blip/exp_coco_ret_eval.yaml
-python -m torch.distributed.run --nproc_per_node=1 evaluate.py --cfg-path lavis/projects/blip/exp_coco_ret_eval.yaml
+python -m torch.distributed.run --nproc_per_node=8 evaluate.py --cfg-path lavis/projects/blip/exp_coco_ret_eval.yaml
