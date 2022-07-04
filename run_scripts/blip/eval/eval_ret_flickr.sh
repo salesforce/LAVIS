0c11391cc66dbbe3bc488a7f0beb5733254cec41
Dongxu Li <dongxu_li@outlook.com>
2022-07-04 02:03:27 +0000

    (refactor, feat) add lavis-trained weights for BLIP large coco caption.

--- run_scripts/blip/eval/eval_ret_flickr.sh
+++ run_scripts/blip/eval/eval_ret_flickr.sh
@@ -1,2 +1,2 @@
 # python -m torch.distributed.run --nproc_per_node=16 evaluate.py --cfg-path lavis/projects/blip/eval/ret_flickr_eval.yaml
-python -m torch.distributed.run --nproc_per_node=1 evaluate.py --cfg-path lavis/projects/blip/eval/ret_flickr_eval.yaml
+python -m torch.distributed.run --nproc_per_node=8 evaluate.py --cfg-path lavis/projects/blip/eval/ret_flickr_eval.yaml
