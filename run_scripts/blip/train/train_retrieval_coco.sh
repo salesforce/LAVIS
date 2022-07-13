f7c8f2e91617886482c4001087d578fdc1cfc4a5
Dongxu Li <dongxu_li@outlook.com>
2022-07-13 07:32:03 +0000

    (minor) tweak on alpro didemo.

--- run_scripts/blip/train/train_retrieval_coco.sh
+++ run_scripts/blip/train/train_retrieval_coco.sh
@@ -1,1 +1,1 @@
-python -m torch.distributed.run --nproc_per_node=8 train.py --cfg-path lavis/projects/blip/exp_coco_ret_ft.yaml
+python -m torch.distributed.run --nproc_per_node=8 train.py --cfg-path lavis/projects/blip/train/retrieval_coco_ft.yaml
