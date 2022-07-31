e212b0f2cc8140816b8dc923254821e24bdc249e
Dongxu Li <dongxu_li@outlook.com>
2022-07-31 09:51:46 +0000

    (fix) correct vg auto-download paths.

--- lavis/datasets/download_scripts/download_vg.py
+++ lavis/datasets/download_scripts/download_vg.py
@@ -12,9 +12,8 @@ from lavis.common.utils import (
 
 
 DATA_URL = {
-    "train": "http://images.cocodataset.org/zips/train2014.zip",  # md5: 0da8c0bd3d6becc4dcb32757491aca88
-    "val": "http://images.cocodataset.org/zips/val2014.zip",  # md5: a3d79f5ed8d289b7a7554ce06a5782b3
-    "test": "http://images.cocodataset.org/zips/test2015.zip",  # md5: 04127eef689ceac55e3a572c2c92f264
+    "train": "https://cs.stanford.edu/people/rak248/VG_100K_2/images.zip",
+    "train2": "https://cs.stanford.edu/people/rak248/VG_100K_2/images2.zip",
 }
 
 
