fe4d144591b9fe05097a964080ae563f5cdb2349
Dongxu Li <dongxu_li@outlook.com>
2022-09-16 09:46:18 +0000

    default cache root.

--- lavis/__init__.py
+++ lavis/__init__.py
@@ -22,8 +22,10 @@ root_dir = os.path.dirname(os.path.abspath(__file__))
 default_cfg = OmegaConf.load(os.path.join(root_dir, "configs/default.yaml"))
 
 registry.register_path("library_root", root_dir)
-registry.register_path("repo_root", os.path.join(root_dir, ".."))
-registry.register_path("cache_root", default_cfg.env.cache_root)
+repo_root = os.path.join(root_dir, "..")
+registry.register_path("repo_root", repo_root)
+cache_root = os.path.join(repo_root, default_cfg.env.cache_root)
+registry.register_path("cache_root", cache_root)
 
 registry.register("MAX_INT", sys.maxsize)
 registry.register("SPLIT_NAMES", ["train", "val", "test"])
