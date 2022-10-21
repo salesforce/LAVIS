73278713fc7a5334796424302ba65b7f21c9c014
anthonytmh <anthony.tiong@salesforce.com>
2022-10-21 03:20:48 +0000

    fix dict key

--- lavis/tasks/vqa.py
+++ lavis/tasks/vqa.py
@@ -153,7 +153,7 @@ class VQATask(BaseTask):
                     "%s : %.02f"
                     % (ans_type, vqa_scorer.accuracy["perAnswerType"][ans_type])
                 )
-                metrics["ansType"] = vqa_scorer.accuracy["perAnswerType"][ans_type]
+                metrics[ans_type] = vqa_scorer.accuracy["perAnswerType"][ans_type]
 
             with open(
                     os.path.join(registry.get_path("output_dir"), "evaluate.txt"), "a"
