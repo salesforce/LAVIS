1118c1b7cdd5c1f78b8520809fdbfbfa1e3cc57c
Dongxu <dongxu_li@outlook.com>
2023-02-13 08:36:15 +0800

    add BLIP2 pretraining (#130)
    * add scripts for blip2 zero-shot vqa&okvqa evaluation
    
    * delete draft task and add back caption evaluation
    
    * fix amp scaler, fix freeze ViT, add blip-2 finetune script
    
    * remove OKVQA task, apply lemmatization after predict_answers().
    
    * fix optimizer zero_grad under amp
    
    * zero-shot gqa evaluation
    
    * Fix https://github.com/salesforce/LAVIS/issues/119.
    
    * update runner - configurable beta and save checkpoint only for requires_grad params
    
    * add blip2 image processor for training
    
    * use blip2 image processor for training
    
    * update blip2 pretraining yaml and add stage-2 pretraining script
    
    * reload checkpoint to cpu first. allow non-strict reloading.
    
    * remove blip2 pretrain.yaml
    
    ---------
    
    Co-authored-by: root <“junnan.li@salesforce.com”>

--- lavis/processors/blip_processors.py
+++ lavis/processors/blip_processors.py
@@ -192,3 +192,48 @@ class BlipImageEvalProcessor(BlipImageBaseProcessor):
         std = cfg.get("std", None)
 
         return cls(image_size=image_size, mean=mean, std=std)
+
+
+@registry.register_processor("blip2_image_train")
+class Blip2ImageTrainProcessor(BlipImageBaseProcessor):
+    def __init__(
+        self, image_size=364, mean=None, std=None, min_scale=0.5, max_scale=1.0
+    ):
+        super().__init__(mean=mean, std=std)
+
+        self.transform = transforms.Compose(
+            [
+                transforms.RandomResizedCrop(
+                    image_size,
+                    scale=(min_scale, max_scale),
+                    interpolation=InterpolationMode.BICUBIC,
+                ),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                self.normalize,
+            ]
+        )
+
+    def __call__(self, item):
+        return self.transform(item)
+
+    @classmethod
+    def from_config(cls, cfg=None):
+        if cfg is None:
+            cfg = OmegaConf.create()
+
+        image_size = cfg.get("image_size", 364)
+
+        mean = cfg.get("mean", None)
+        std = cfg.get("std", None)
+
+        min_scale = cfg.get("min_scale", 0.5)
+        max_scale = cfg.get("max_scale", 1.0)
+
+        return cls(
+            image_size=image_size,
+            mean=mean,
+            std=std,
+            min_scale=min_scale,
+            max_scale=max_scale,
+        )
