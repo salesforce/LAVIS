faf794207e564d3054c7fac7d7fa783b9aa54a4f
root <junnan.li@salesforce.com>
2023-02-16 08:11:38 +0000

    add coyo webdataset

--- lavis/datasets/builders/image_text_pair_builder.py
+++ lavis/datasets/builders/image_text_pair_builder.py
@@ -11,6 +11,7 @@ from lavis.common.registry import registry
 from lavis.datasets.builders.base_dataset_builder import BaseDatasetBuilder
 from lavis.datasets.datasets.image_text_pair_datasets import ImageTextPairDataset
 from lavis.datasets.datasets.laion_dataset import LaionDataset
+from lavis.datasets.datasets.coyo_dataset import CoyoDataset
 
 
 @registry.register_builder("conceptual_caption_3m")
@@ -50,6 +51,37 @@ class Laion2BMultiBuilder(BaseDatasetBuilder):
     train_dataset_cls = LaionDataset
 
     DATASET_CONFIG_DICT = {"default": "configs/datasets/laion/defaults_2B_multi.yaml"}
+
+    def _download_ann(self):
+        pass
+
+    def _download_vis(self):
+        pass
+
+    def build(self):
+        self.build_processors()
+
+        build_info = self.config.build_info
+
+        datasets = dict()
+        split = "train"  # laion dataset only has train split
+
+        # create datasets
+        # [NOTE] return inner_datasets (wds.DataPipeline)
+        dataset_cls = self.train_dataset_cls
+        datasets[split] = dataset_cls(
+            vis_processor=self.vis_processors[split],
+            text_processor=self.text_processors[split],
+            location=build_info.storage,
+        ).inner_dataset
+
+        return datasets
+    
+@registry.register_builder("coyo")
+class CoyoBuilder(BaseDatasetBuilder):
+    train_dataset_cls = CoyoDataset
+
+    DATASET_CONFIG_DICT = {"default": "configs/datasets/coyo/defaults_700m_capfilt.yaml"}
 
     def _download_ann(self):
         pass
