ab039d9ff5f957347aa3085f80076117e0453141
wenliangdai <wdaiai@connect.ust.hk>
2023-03-03 06:16:14 +0000

    Improved COCOCapInstructDataset implementation

--- lavis/datasets/datasets/instruct_wrappers/coco_caption_instruct_datasets.py
+++ lavis/datasets/datasets/instruct_wrappers/coco_caption_instruct_datasets.py
@@ -1,3 +1,7 @@
+from lavis.datasets.datasets.caption_datasets import CaptionDataset
 from lavis.datasets.datasets.instruct_wrappers.caption_instruct_datasets import CaptionDatasetInstructWrapper
 
-COCOCapInstructDataset = CaptionDatasetInstructWrapper
+class COCOCapInstructDataset(CaptionDatasetInstructWrapper):
+    def __init__(self, vis_processor, text_processor, vis_root, ann_paths, instruction_path):
+        super().__init__(vis_processor, text_processor, vis_root, ann_paths, instruction_path)
+        self.dataset = CaptionDataset(vis_processor, text_processor, vis_root, ann_paths)
