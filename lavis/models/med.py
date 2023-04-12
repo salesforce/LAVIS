537a1defcac9e08a702ebd3a246edc9fc3a068c6
Bo Li <drluodian@gmail.com>
2023-04-12 12:04:09 +0000

    fix on med.py: add determine statement before repeat tokenized_prompt.

--- lavis/models/med.py
+++ lavis/models/med.py
@@ -1331,7 +1331,9 @@ class XBertLMHeadDecoder(BertLMHeadModel):
         if not use_nucleus_sampling:
             num_beams = num_beams
             visual_embeds = visual_embeds.repeat_interleave(num_beams, dim=0)
-            tokenized_prompt.input_ids = tokenized_prompt.input_ids.repeat_interleave(num_beams, dim=0)
+
+            if visual_embeds.size(0) != tokenized_prompt.input_ids.size(0):
+                tokenized_prompt.input_ids = tokenized_prompt.input_ids.repeat_interleave(num_beams, dim=0)
             # Make sure that the prompt is repeated same number of times as the visual_embeds
             assert visual_embeds.size(0) == tokenized_prompt.input_ids.size(0)
 
